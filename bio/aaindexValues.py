cf095deb77b1f2e6a44ab31ea28cb9bba92f239d
javafalcon <javafalcon@163.com>
2020-05-26 07:49:23 +0800

    5-26-20

--- bio/aaindexValues.py
+++ bio/aaindexValues.py
@@ -5,7 +5,8 @@ Created on Sun May  3 16:34:55 2020
 @author: lwzjc
 """
 import numpy as np
-
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 AminoAcids = 'ARNDCQEGHILKMFPSTWYV'
 def aaindex1Values():
     AAValues=[]
@@ -35,5 +36,23 @@ def aaindex1Values():
                     t.append(eval(v))
     return np.array(AAValues)      
 
+def aaindex1PCAValues():
+    with open('E:\\Repoes\\jci\\bio\\Amino_Acids_PCAVal_dict.txt','r') as fr:
+        aadic = eval(fr.read())
+    return aadic
+        
 if __name__ == '__main__':
     aavals = aaindex1Values()
+    
+    scaler = StandardScaler()
+    aa_scal = scaler.fit_transform(aavals)
+    pca = PCA(n_components=20)
+    aa_pca = pca.fit_transform(aa_scal.T)
+    aaval_dic = {}
+    for i in range(20):
+        aaval_dic[AminoAcids[i]] = list(aa_pca[i])
+    with open('Amino_Acids_PCAVal_dict.txt','w') as fw:
+        fw.write(str(aaval_dic))
+    
+        
+    
