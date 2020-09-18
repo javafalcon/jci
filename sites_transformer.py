6083cc813d97cb6a82d1cbaaee6275777c77dc63
javafalcon <javafalcon@163.com>
2020-09-18 09:37:50 +0800

    2020-9-18

--- sites_transformer.py
+++ sites_transformer.py
@@ -147,13 +147,13 @@ class EncoderLayer(layers.Layer):
 
 class Encoder(layers.Layer):
     def __init__(self, n_layers, d_model, n_heads, ffd,
-                 input_vocab_size, max_seq_len, dropout_rate=0.1):
+                 seq_len, input_vocab_size, dropout_rate=0.1):
         super(Encoder, self).__init__()
         
         self.n_layers = n_layers
         self.d_model = d_model
         self.embedding = layers.Embedding(input_vocab_size, d_model)
-        self.pos_embedding = position_encoding(max_seq_len, d_model)
+        self.pos_embedding = position_encoding(seq_len//2, d_model)
         self.encoder_layer = [EncoderLayer(d_model, n_heads, ffd, dropout_rate)
                               for _ in range(n_layers)]
         self.dropout = layers.Dropout(dropout_rate)
@@ -161,10 +161,10 @@ class Encoder(layers.Layer):
     def call(self, inputs, training, mask):
         seq_len = inputs.shape[-1]
         word_emb = self.embedding(inputs)       
-        word_emb = tf.cumsum(word_emb, axis=1)
         #word_emb *= (tf.cast(self.d_model, tf.float32))
         emb = word_emb + self.pos_embedding[:, :seq_len, :]
         x = self.dropout(emb, training=training)
+    
         for i in range(self.n_layers):
             x = self.encoder_layer[i](x, training, mask)
         return x
