cf3471eb7899fe4118f62112d95596dd222bc30c
javafalcon <javafalcon@163.com>
2020-11-16 15:12:19 +0800

    11-16

--- Capsule.py
+++ Capsule.py
@@ -110,4 +110,16 @@ class Mask(layers.Layer):
         if type(input_shape[0]) is tuple: # true lable provided
             return tuple([None, input_shape[0][-1]])
         else:
-            return tuple([None, input_shape[-1]])    
+            return tuple([None, input_shape[-1]])   
+def PrimaryCap(inputs, dim_vector, n_channels, kernel_size, strides, padding, name='primarycap'):
+    """
+    Apply Conv2D `n_channels` times and concatenate all capsules
+    :param inputs: 4D tensor, shape=[None, width, height, channels]
+    :param dim_vector: the dim of the output vector of capsule
+    :param n_channels: the number of types of capsules
+    :return: output tensor, shape=[None, num_capsule, dim_vector]
+    """
+    output = layers.Conv2D(filters=dim_vector*n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
+                           name=name+'_conv2d')(inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_vector], name=name+'_reshape')(output)
+    return layers.Lambda(squash, name=name+'_squash')(outputs)
